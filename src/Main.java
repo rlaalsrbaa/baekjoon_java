289e70a333bcd60cce1d1ab0adad725f5f5ea1fc
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-07-07 19:59:16 +0900

    백준 1단계 10718번 We love kriii

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.print("Hello World!");
+    System.out.println("강한친구 대한육군");
+    System.out.println("강한친구 대한육군");
   }
 }
