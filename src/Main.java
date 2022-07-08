cdb0d7a19265e6e0f19521cc58fbb83d152afa33
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-07-08 23:58:44 +0900

    백준 1단계 10171번 개

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("\\    /\\");
-    System.out.println(" )  ( ')");
-    System.out.println("(  /  )");
-    System.out.println(" \\(__)|\n");
+    System.out.println("|\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.print("||_/=\\\\__|");
   }
 }
