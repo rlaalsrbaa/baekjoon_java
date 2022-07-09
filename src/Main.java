a2ebdd72e98f69c392c2d3d1ec4fdeda22a9d259
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-07-09 18:33:27 +0900

    백준 1단계 1000번 A+B

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("|\\_/|");
-    System.out.println("|q p|   /}");
-    System.out.println("( 0 )\"\"\"\\");
-    System.out.println("|\"^\"`    |");
-    System.out.print("||_/=\\\\__|");
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+
+    System.out.println(A+B);
+
+    sc.close();
   }
 }
