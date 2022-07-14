741259492f6f0de7f0ecaa69dc8f9a7f95c7463b
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-07-14 21:52:09 +0900

    백준 1단계 10430번 나눗셈

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int year = sc.nextInt();
-
-    System.out.printf("%d",year-543);
-
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    int C = sc.nextInt();
+    System.out.printf("%d\n",(A+B)%C);
+    System.out.printf("%d\n",((A%C)+(B%C))%C);
+    System.out.printf("%d\n",(A*B)%C);
+    System.out.printf("%d\n",((A%C)*(B%C))%C);
 
     sc.close();
   }
