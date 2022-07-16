9c8c1af7675116f1722ed656698cc07dd34db1ad
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-07-16 21:52:56 +0900

    백준 1단계 25083번 새싹

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int A = sc.nextInt();
-    int B = sc.nextInt();
 
-    System.out.println(A*(B%10));
-    System.out.println(A*((B%100)/10));
-    System.out.println(A*((B%1000)/100));
-    System.out.println(A*B);
-    sc.close();
+    System.out.println("         ,r'\"7");
+    System.out.println("r`-_   ,'  ,/");
+    System.out.println(" \\. \". L_r'");
+    System.out.println("   `~\\/");
+    System.out.println("      |");
+    System.out.println("      |");
+
   }
 }
