8bc48a61d18d525df7d4d7612e431a3280b289a3
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-07-20 18:59:06 +0900

    백준 3단계 10950번 A+B -3

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int A = sc.nextInt();
+    int T = sc.nextInt();
 
-    for (int i = 1;i<=9;i++){
-
-      System.out.printf("%d * %d = %d\n",A,i,A*i);
+    for (int i = 1;i<=T;i++){
+      int A = sc.nextInt();
+      int B = sc.nextInt();
+      System.out.printf("%d\n", A + B);
     }
 
     sc.close();
