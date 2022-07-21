dee228fed8748cd0aacb79568874f8e491522c66
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-07-21 21:01:40 +0900

    백준 3단계 8393번 합

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt();
-
-    for (int i = 1;i<=T;i++){
-      int A = sc.nextInt();
-      int B = sc.nextInt();
-      System.out.printf("%d\n", A + B);
+    int n = sc.nextInt();
+    int sum = 0;
+    for (int i = 1;i<=n;i++){
+      sum = sum + i;
     }
-
+    System.out.println(sum);
     sc.close();
   }
 }
