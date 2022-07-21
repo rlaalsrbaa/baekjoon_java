831a739d4f26ee59d7a6b0d918452aed26b8188d
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-07-21 21:21:19 +0900

    백준 3단계 15552번 빠른 A+B

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int sum = 0;
-    for (int i = 1;i<=n;i++){
-      sum = sum + i;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int T = Integer.parseInt(br.readLine());
+
+    for(int i = 0; i < T; i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int A = Integer.parseInt(st.nextToken());
+      int B = Integer.parseInt(st.nextToken());
+      bw.write((A+B) +"\n");
     }
-    System.out.println(sum);
-    sc.close();
+
+    bw.flush();
+    br.close();
+    bw.close();
+
   }
 }
