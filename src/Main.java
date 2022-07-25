eb96c37c5398cb5b09de561fb90c01a90a382d80
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-07-25 23:05:10 +0900

    백준 3단계 10871번 X보다 작은 수

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    int T = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int X = Integer.parseInt(st.nextToken());
 
+    StringTokenizer st2 = new StringTokenizer(br.readLine());
 
-    for(int i = 1; i <= T; i++){
-      String star = "*";
-      String repeat = star.repeat(i);
-      for(int j=T-1; j >= i; j--){
-        bw.write(" ");
+    for(int i = 1; i <= N; i++){
+      int A = Integer.parseInt(st2.nextToken());
+      if(A < X){
+        bw.write(A + " ");
       }
-      bw.write(String.format("%s\n",repeat));
     }
 
     bw.flush();
