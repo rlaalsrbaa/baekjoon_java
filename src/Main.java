db4546b36e3dcb13e2500fc6a6849530a660b544
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-01 22:48:14 +0900

    백준 4단계 2577번 숫자의 개수

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 import java.io.*;
-import java.util.StringTokenizer;
-import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    int[] arr = new int[9];
-    int max = 0;
-    int des = 1;
+    int A = Integer.parseInt(br.readLine());
+    int B = Integer.parseInt(br.readLine());
+    int C = Integer.parseInt(br.readLine());
+    int cnt = 0;
+    String mul = String.valueOf(A * B * C);
+    char[] arr = mul.toCharArray();
 
-    for(int i = 0; i < 9; i++){
-      arr[i] = Integer.parseInt(br.readLine());
-      if(i == 0){
-        max = arr[0];
+    for(int i = 0; i <= 9; i++){
+      for(int j = 0; j < arr.length; j++){
+        if(arr[j]-'0' == i){
+          cnt++;
+        }
       }
-      else if(max < arr[i]){
-        max = arr[i];
-        des = i+1;
-      }
+      bw.write(String.format("%d\n",cnt));
+      cnt = 0;
     }
 
-    bw.write(String.format("%d\n",max));
-    bw.write(String.format("%d\n",des));
+
 
     bw.flush();
     br.close();
