39f5c59295c6fc9b40d16272b83ff5a5c8f31c04
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-01 23:23:45 +0900

    백준 4단계 3052번 나머지

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,26 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    int A = Integer.parseInt(br.readLine());
-    int B = Integer.parseInt(br.readLine());
-    int C = Integer.parseInt(br.readLine());
-    int cnt = 0;
-    String mul = String.valueOf(A * B * C);
-    char[] arr = mul.toCharArray();
+    int cnt = 10;
+    int[] arr = new int[10];
 
     for(int i = 0; i <= 9; i++){
-      for(int j = 0; j < arr.length; j++){
-        if(arr[j]-'0' == i){
-          cnt++;
-        }
-      }
-      bw.write(String.format("%d\n",cnt));
-      cnt = 0;
+      arr[i] = Integer.parseInt(br.readLine()) % 42;
     }
 
+    Arrays.sort(arr);
 
+    for(int i = 0; i <= 9; i++){
+      if(i > 0 && arr[i] % 42 == arr[i-1] % 42){
+        cnt--;
+      }
+    }
 
+    bw.write(Integer.toString(cnt));
     bw.flush();
     br.close();
     bw.close();
