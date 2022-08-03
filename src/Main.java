ffd1427d27f122282d6afef71f2bc1fdd35b7dd7
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-03 20:59:48 +0900

    백준 4단계 3052번 나머지

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,32 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     int N = Integer.parseInt(br.readLine());
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    float[] arr = new float[N];
-    float max = 0;
-    float res = 0;
+    int cnt = 0;
+    int score = 0;
 
     for(int i = 0; i < N; i++){
-      arr[i] = Integer.parseInt(st.nextToken());
+      String[] str = new String[N];
+      str[i] = br.readLine();
+
+      for(int j = 0; j < str[i].length(); j++){
+        if(str[i].charAt(j) == 'O'){
+          cnt++;
+          score += cnt;
+        }
+        else{
+          cnt = 0;
+        }
+      }
+
+      bw.write(Integer.toString(score)+"\n");
+      score = 0;
+      cnt = 0;
     }
-    Arrays.sort(arr);
-    max = arr[arr.length-1];
-    for(int i = 0; i < N; i++){
-      arr[i] = arr[i] / max * 100;
-      res += arr[i];
-    }
-    res = res / N;
-    System.out.println(res);
+
     bw.flush();
     br.close();
     bw.close();
