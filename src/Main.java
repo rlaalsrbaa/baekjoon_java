1f0e074bd1211f1f1537eff74d0d2f9e1d203755
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-04 23:17:22 +0900

    백준 4단계 4344번 평균은 넘겠지

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,48 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    int N = Integer.parseInt(br.readLine());
-    int cnt = 0;
-    int score = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int C = Integer.parseInt(br.readLine());
+        int N = 0;
+        int sum = 0;
+        float avg = 0;
+        float[] arr;
+        int cnt = 0;
 
-    for(int i = 0; i < N; i++){
-      String[] str = new String[N];
-      str[i] = br.readLine();
+        for (int i = 0; i < C; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            arr = new float[N];
 
-      for(int j = 0; j < str[i].length(); j++){
-        if(str[i].charAt(j) == 'O'){
-          cnt++;
-          score += cnt;
+            for (int j = 0; j < N; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+                sum += arr[j];
+
+                if (j == N-1) {
+                    avg = sum / N;
+
+                    for (int k = 0; k < N; k++) {
+
+                        if (arr[k] > avg) {
+                            cnt++;
+                        }
+
+                    }
+                    avg = (float) cnt / (float) N;
+                    sum = 0;
+                    cnt = 0;
+                }
+
+            }
+            bw.write(String.format("%.3f%%\n", avg * 100));
         }
-        else{
-          cnt = 0;
-        }
-      }
 
-      bw.write(Integer.toString(score)+"\n");
-      score = 0;
-      cnt = 0;
+        bw.flush();
+        br.close();
+        bw.close();
+
     }
-
-    bw.flush();
-    br.close();
-    bw.close();
-
-  }
 }
