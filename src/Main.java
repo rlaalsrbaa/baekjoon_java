1319e83ecceafa44f0058dfff9f1a8da1bfef68a
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-04 23:18:10 +0900

    백준 5단계 15596번 정수 N개의 합

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,10 @@
-import java.io.*;
-import java.util.StringTokenizer;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int C = Integer.parseInt(br.readLine());
-        int N = 0;
-        int sum = 0;
-        float avg = 0;
-        float[] arr;
-        int cnt = 0;
-
-        for (int i = 0; i < C; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            arr = new float[N];
-
-            for (int j = 0; j < N; j++) {
-                arr[j] = Integer.parseInt(st.nextToken());
-                sum += arr[j];
-
-                if (j == N-1) {
-                    avg = sum / N;
-
-                    for (int k = 0; k < N; k++) {
-
-                        if (arr[k] > avg) {
-                            cnt++;
-                        }
-
-                    }
-                    avg = (float) cnt / (float) N;
-                    sum = 0;
-                    cnt = 0;
-                }
-
-            }
-            bw.write(String.format("%.3f%%\n", avg * 100));
+class Test{
+    public long sum(int[] a){
+        long sum = 0;
+        for(int i = 0; i < a.length; i++){
+            sum += a[i];
         }
-
-        bw.flush();
-        br.close();
-        bw.close();
-
+        return sum;
     }
+
 }
