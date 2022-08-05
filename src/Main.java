ba7e87e7a1f478759435bcdb592ac21b89b752d5
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-05 23:55:59 +0900

    백준 5단계 1065번 한수

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,40 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int con = 0;
-        List<Integer> arr = new ArrayList<>();
-
-        for(int i = 1; i <= 10000; i++){
-            String str = Integer.toString(i);
-
-            for(int j = 0; j < str.length(); j++){
-
-                con += (str.charAt(j) - 48);
-            }
-
-            arr.add(con + i);
-            con = 0;
-        }
-
-        for (int i = 1; i <= 10000; i++){
-            if(arr.contains(i)){
-                continue;
-            }
-            bw.write(Integer.toString(i)+"\n");
-        }
-
-        bw.flush();
-        br.close();
-        bw.close();
-
+        int qst = Integer.parseInt(br.readLine());
+        solution(qst);
     }
 
+    public static void solution(int qst) {
+        int cnt = 0;
+        String str;
+        int a;
+        int b;
+        int c;
+        for (int i = 1; i <= qst; i++) {
 
+            if (i < 100) {
+                cnt++;
+                continue;
+            }
+            str = Integer.toString(i);
+            a = str.charAt(0)-48;
+            b = str.charAt(1)-48;
+            c = str.charAt(2)-48;
+            if(a == b && b == c){
+                cnt++;
+            }
+            else if(a < b && b < c && (b - a) == (c - b)){
+                cnt++;
+            }
+            else if(a > b && b > c && (a - b)==(b - c)){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
