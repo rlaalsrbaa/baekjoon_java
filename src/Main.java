7b38199d9a3091e58f755a94e2702938af61fa19
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-06 08:43:56 +0900

    백준 6단계 11720번 숫자의 합

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
         String str = br.readLine();
+        int sum = 0;
 
-        bw.write(Integer.toString(str.charAt(0)));
+        for(int i = 0; i < N; i++){
+            sum += str.charAt(i) - '0';
+
+        }
+
+        bw.write(Integer.toString(sum));
 
         bw.flush();
         bw.close();
         br.close();
     }
 
-}1
+}
