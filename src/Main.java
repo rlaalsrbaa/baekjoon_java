aa4adc64e06a92874c0d1222eb3a111203550467
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-06 08:27:25 +0900

    백준 6단계 11654번 아스키 코드

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int qst = Integer.parseInt(br.readLine());
-        solution(qst);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+
+        bw.write(Integer.toString(str.charAt(0)));
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    public static void solution(int qst) {
-        int cnt = 0;
-        String str;
-        int a;
-        int b;
-        int c;
-        for (int i = 1; i <= qst; i++) {
-
-            if (i < 100) {
-                cnt++;
-                continue;
-            }
-            str = Integer.toString(i);
-            a = str.charAt(0)-48;
-            b = str.charAt(1)-48;
-            c = str.charAt(2)-48;
-            if(a == b && b == c){
-                cnt++;
-            }
-            else if(a < b && b < c && (b - a) == (c - b)){
-                cnt++;
-            }
-            else if(a > b && b > c && (a - b)==(b - c)){
-                cnt++;
-            }
-        }
-        System.out.println(cnt);
-    }
-}
+}1
