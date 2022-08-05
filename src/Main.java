d71591c651914f3eeef9375faeeed4aacb15acde
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-05 23:21:50 +0900

    백준 5단계 4673번 셀프 넘버

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,38 @@
-class Test{
-    public long sum(int[] a){
-        long sum = 0;
-        for(int i = 0; i < a.length; i++){
-            sum += a[i];
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int con = 0;
+        List<Integer> arr = new ArrayList<>();
+
+        for(int i = 1; i <= 10000; i++){
+            String str = Integer.toString(i);
+
+            for(int j = 0; j < str.length(); j++){
+
+                con += (str.charAt(j) - 48);
+            }
+
+            arr.add(con + i);
+            con = 0;
         }
-        return sum;
+
+        for (int i = 1; i <= 10000; i++){
+            if(arr.contains(i)){
+                continue;
+            }
+            bw.write(Integer.toString(i)+"\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+
     }
+
 
 }
