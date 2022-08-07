2821097ceec6cd28f6d1f2fda0bfe40f66ddf23a
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-07 21:08:21 +0900

    백준 6단계 10809번 알파벳 찾기

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        String str = br.readLine();
-        int sum = 0;
 
-        for(int i = 0; i < N; i++){
-            sum += str.charAt(i) - '0';
+        String S = br.readLine();
+        char[] arr = S.toCharArray();
+        int des = 0;
+
+        for (int i = 97; i <= 122; i++) {
+
+            for (int j = 0; j < S.length(); j++) {
+
+                if (arr[j] == i) {
+                    bw.write(Integer.toString(des) + " ");
+                    des = 0;
+                    break;
+
+                } else if (j == S.length() - 1) {
+                    bw.write("-1 ");
+                    des = 0;
+                    break;
+                }
+                des++;
+
+            }
+
 
         }
-
-        bw.write(Integer.toString(sum));
 
         bw.flush();
         bw.close();
         br.close();
-    }
 
+    }
 }
