34da89534e2dc14eac5bd9e88b2a13c864afa99d
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-07 23:44:47 +0900

    백준 6단계 2675번 문자열 반복

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,32 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder P = new StringBuilder();
 
-        String S = br.readLine();
-        char[] arr = S.toCharArray();
-        int des = 0;
+        for(int i = 0; i < T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int R = Integer.parseInt(st.nextToken());
+            String S = st.nextToken();
 
-        for (int i = 97; i <= 122; i++) {
+            P.setLength(0);
 
-            for (int j = 0; j < S.length(); j++) {
-
-                if (arr[j] == i) {
-                    bw.write(Integer.toString(des) + " ");
-                    des = 0;
-                    break;
-
-                } else if (j == S.length() - 1) {
-                    bw.write("-1 ");
-                    des = 0;
-                    break;
+            for(int j = 0; j < S.length(); j++){
+                for(int k = 0; k < R; k++){
+                    P.append(S.charAt(j));
                 }
-                des++;
-
             }
 
-
+            bw.write(P.toString());
+            bw.write("\n");
         }
 
         bw.flush();
         bw.close();
         br.close();
-
     }
 }
