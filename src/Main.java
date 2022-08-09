0067d91477c729c7d3c08998727fef8eea151fd6
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-10 01:01:38 +0900

    백준 6단계 1157번 단어 공부 풀이1

--- src/Main.java
+++ src/Main.java
@@ -2,49 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        char[] inputWord = br.readLine().toCharArray();
-        get_Convert_UpperCase(inputWord);
-
-        List<Character> arr = new ArrayList<>();
-        HashSet<Character> removeDupWord = new HashSet<>();
-
-        for (char var : inputWord
-        ) {
-            removeDupWord.add(var);
-            arr.add(var);
-        }
-        Character[] removedDupArray = removeDupWord.toArray(new Character[0]);
-
-        HashMap<Character, Integer> answer = new HashMap<>();
-
-        for(int i = 0; i < removedDupArray.length; i++){
-            char key = removedDupArray[i];
-            int value = Collections.frequency(arr,removedDupArray[i]);
-            answer.put(key,value);
-        }
-        // 밸류값으로 키를 찾으면 됨
-
-
-        int maxValue = Collections.max(answer.values());
-        int[] arrayValues = new int[answer.size()];
-        int cnt = 0;
-        for(int i = 0; i < arrayValues.length; i++){
-
-            if(arrayValues[i] == maxValue && cnt != 0){
-                cnt++;
-                bw.write("?");
-            }
-
-        }
-        bw.flush();
-        bw.close();
-        br.close();
+        solution();
     }
-
     public static char[] get_Convert_UpperCase(char[] arr) {
         for (int i = 0; i < arr.length; i++) {
 
@@ -54,8 +15,79 @@ public class Main {
         }
         return arr;
     }
+    public static List<Character> get_ArrayList_Words(char[] inputWord){
+        List<Character> arr = new ArrayList<>();
+        for (char var : inputWord
+        ) {
 
-    public static void Solution(char[] arr) {
-
+            arr.add(var);
+        }
+        return arr;
     }
+    public static Character[] get_Removed_Duplicate_Array(char[] inputWord){
+
+        HashSet<Character> removeDupWord = new HashSet<>();
+
+        for (char var : inputWord
+        ) {
+            removeDupWord.add(var);
+
+        }
+
+        Character[] removedDupArray = removeDupWord.toArray(new Character[0]);
+
+        return removedDupArray;
+    }
+    public static char[] get_Input_Word() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] inputWord = br.readLine().toCharArray();
+        br.close();
+        return inputWord;
+    }
+    private static HashMap<Character, Integer> get_Frequency_Words(Character[] removedDupArray, List<Character> wordsArrayList){
+        HashMap<Character, Integer> answer = new HashMap<>();
+        for(int i = 0; i < removedDupArray.length; i++){
+            char key = removedDupArray[i];
+            int value = Collections.frequency(wordsArrayList,removedDupArray[i]);
+            answer.put(key,value);
+        }
+        return answer;
+    }
+    public static void solution() throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] inputWord = get_Input_Word();
+
+        get_Convert_UpperCase(inputWord);
+
+        Character[] removedDupArray = get_Removed_Duplicate_Array(inputWord);
+
+        List<Character> wordsArrayList = get_ArrayList_Words(inputWord);
+
+        HashMap<Character, Integer> wordsMapping = get_Frequency_Words(removedDupArray,wordsArrayList);
+
+        char answer = get_Answer(wordsMapping);
+
+        bw.write(answer);
+        bw.flush();
+        bw.close();
+    }
+    public static char get_Answer(HashMap<Character, Integer> wordsMapping){
+        int maxValue = Collections.max(wordsMapping.values());
+        char answer = 0;
+        int cnt = 0;
+
+        for (Map.Entry<Character, Integer> var: wordsMapping.entrySet()
+        ) {
+            if(var.getValue() == maxValue){
+                answer = var.getKey();
+                cnt++;
+            }
+        }
+        if(cnt > 1){
+            answer = '?';
+        }
+        return answer;
+    }
+
 }
