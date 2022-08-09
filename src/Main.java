b8b135beb1445219ffc51422d445a0323b90dd95
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-09 23:55:27 +0900

    백준 6단계 1157번 단어공부 해결과정

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,61 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder P = new StringBuilder();
 
-        for(int i = 0; i < T; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int R = Integer.parseInt(st.nextToken());
-            String S = st.nextToken();
+        char[] inputWord = br.readLine().toCharArray();
+        get_Convert_UpperCase(inputWord);
 
-            P.setLength(0);
+        List<Character> arr = new ArrayList<>();
+        HashSet<Character> removeDupWord = new HashSet<>();
 
-            for(int j = 0; j < S.length(); j++){
-                for(int k = 0; k < R; k++){
-                    P.append(S.charAt(j));
-                }
+        for (char var : inputWord
+        ) {
+            removeDupWord.add(var);
+            arr.add(var);
+        }
+        Character[] removedDupArray = removeDupWord.toArray(new Character[0]);
+
+        HashMap<Character, Integer> answer = new HashMap<>();
+
+        for(int i = 0; i < removedDupArray.length; i++){
+            char key = removedDupArray[i];
+            int value = Collections.frequency(arr,removedDupArray[i]);
+            answer.put(key,value);
+        }
+        // 밸류값으로 키를 찾으면 됨
+
+
+        int maxValue = Collections.max(answer.values());
+        int[] arrayValues = new int[answer.size()];
+        int cnt = 0;
+        for(int i = 0; i < arrayValues.length; i++){
+
+            if(arrayValues[i] == maxValue && cnt != 0){
+                cnt++;
+                bw.write("?");
             }
 
-            bw.write(P.toString());
-            bw.write("\n");
         }
-
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    public static char[] get_Convert_UpperCase(char[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+
+            if ((int) arr[i] > 91) {
+                arr[i] -= 32;
+            }
+        }
+        return arr;
+    }
+
+    public static void Solution(char[] arr) {
+
     }
 }
