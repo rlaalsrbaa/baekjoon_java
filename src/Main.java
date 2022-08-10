a2f9c0eb5969efe700ca93b2fd11264216469d24
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-11 01:03:30 +0900

    백준 6단계 1157번 단어 공부 풀이2 작성중

--- src/Main.java
+++ src/Main.java
@@ -1,93 +1,42 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
+    public static List<Character> get_UpperCase_Word_List(char[] word){
+        List<Character> wordList = new ArrayList<>();
+        for (char var: word
+        ) {
+            if((int)var > 91){
+                var -= 32;
+            }
+            wordList.add(var);
+        }
+        return wordList;
+    }
+    public static char[] get_Word_Array() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] word = br.readLine().toCharArray();
+        br.close();
+        return word;
+    }
+    public static void solution() throws IOException{
+        char[] wordArray = get_Word_Array();
+        List<Character> wordList = get_UpperCase_Word_List(wordArray);
+        String str = "";
+
+        for(int i = 0; i < wordList.size(); i++ ){
+            if(){
+
+            }
+            Collections.frequency(wordList,wordList.get(i));
+        }
+    }
     public static void main(String[] args) throws IOException {
         solution();
     }
-    public static char[] get_Convert_UpperCase(char[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-
-            if ((int) arr[i] > 91) {
-                arr[i] -= 32;
-            }
-        }
-        return arr;
-    }
-    public static List<Character> get_ArrayList_Words(char[] inputWord){
-        List<Character> arr = new ArrayList<>();
-        for (char var : inputWord
-        ) {
-
-            arr.add(var);
-        }
-        return arr;
-    }
-    public static Character[] get_Removed_Duplicate_Array(char[] inputWord){
-
-        HashSet<Character> removeDupWord = new HashSet<>();
-
-        for (char var : inputWord
-        ) {
-            removeDupWord.add(var);
-
-        }
-
-        Character[] removedDupArray = removeDupWord.toArray(new Character[0]);
-
-        return removedDupArray;
-    }
-    public static char[] get_Input_Word() throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] inputWord = br.readLine().toCharArray();
-        br.close();
-        return inputWord;
-    }
-    private static HashMap<Character, Integer> get_Frequency_Words(Character[] removedDupArray, List<Character> wordsArrayList){
-        HashMap<Character, Integer> answer = new HashMap<>();
-        for(int i = 0; i < removedDupArray.length; i++){
-            char key = removedDupArray[i];
-            int value = Collections.frequency(wordsArrayList,removedDupArray[i]);
-            answer.put(key,value);
-        }
-        return answer;
-    }
-    public static void solution() throws IOException{
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        char[] inputWord = get_Input_Word();
-
-        get_Convert_UpperCase(inputWord);
-
-        Character[] removedDupArray = get_Removed_Duplicate_Array(inputWord);
-
-        List<Character> wordsArrayList = get_ArrayList_Words(inputWord);
-
-        HashMap<Character, Integer> wordsMapping = get_Frequency_Words(removedDupArray,wordsArrayList);
-
-        char answer = get_Answer(wordsMapping);
-
-        bw.write(answer);
-        bw.flush();
-        bw.close();
-    }
-    public static char get_Answer(HashMap<Character, Integer> wordsMapping){
-        int maxValue = Collections.max(wordsMapping.values());
-        char answer = 0;
-        int cnt = 0;
-
-        for (Map.Entry<Character, Integer> var: wordsMapping.entrySet()
-        ) {
-            if(var.getValue() == maxValue){
-                answer = var.getKey();
-                cnt++;
-            }
-        }
-        if(cnt > 1){
-            answer = '?';
-        }
-        return answer;
-    }
-
 }
