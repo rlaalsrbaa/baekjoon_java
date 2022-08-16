4e3358319d18f38371697a90706838a5d82611fd
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-16 18:42:30 +0900

    백준 6단계 2908번 상수 풀이1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main { public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer st = new StringTokenizer(br.readLine());
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    bw.write(Integer.toString(st.countTokens()));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    bw.flush();
-    bw.close();
-    br.close();
+        StringBuffer sb1 = new StringBuffer(st.nextToken());
+        StringBuffer sb2 = new StringBuffer(st.nextToken());
+
+        int num1 = Integer.parseInt(sb1.reverse().toString());
+        int num2 = Integer.parseInt(sb2.reverse().toString());
+
+        if (num1 > num2) {
+            bw.write(Integer.toString(num1));
+        } else {
+            bw.write(Integer.toString(num2));
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
