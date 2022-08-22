b0795d402bbfdaf7d9fec920a87d6faafb3e6bec
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-22 23:06:58 +0900

    백준 6단계 5622번 다이얼 풀이 1

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,45 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        StringBuffer sb1 = new StringBuffer(st.nextToken());
-        StringBuffer sb2 = new StringBuffer(st.nextToken());
-
-        int num1 = Integer.parseInt(sb1.reverse().toString());
-        int num2 = Integer.parseInt(sb2.reverse().toString());
-
-        if (num1 > num2) {
-            bw.write(Integer.toString(num1));
-        } else {
-            bw.write(Integer.toString(num2));
+        String str = br.readLine();
+        int time = 0;
+        for(int i = 0; i < str.length(); i++){
+            char var = str.charAt(i);
+            time += getTime(var);
         }
+
+
+        bw.write(Integer.toString(time));
 
         bw.flush();
         bw.close();
         br.close();
+    }
+    public static int getTime(char a){
+        int time = 0;
+        if(a >= 65 && a <= 90){
+            if(a <= 67){ // ABC - 2
+                time = 3;
+            }else if(a <= 70){ // DEF - 3
+                time = 4;
+            }else if(a <= 73){ // GHI - 4
+                time = 5;
+            }else if(a <= 76){ // JKL - 5
+                time = 6;
+            }else if(a <= 79){ // MNO - 6
+                time = 7;
+            }else if(a <= 83){ // PQRS - 7
+                time = 8;
+            }else if(a <= 86){ // TUV - 8
+                time = 9;
+            }else{ // WXYZ - 9
+                time = 10;
+            }
+        }
+        return time;
     }
 }
