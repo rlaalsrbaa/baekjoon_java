b63e70d9e6368384aca55b908adf3b6af60a23e8
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-23 23:54:42 +0900

    백준 6단계 2941번 크로아티아 알파벳 풀이중

--- src/Main.java
+++ src/Main.java
@@ -6,40 +6,60 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String str = br.readLine();
-        int time = 0;
+        String str2 = "";
+        String alphabet = "";
+        int cnt = 0;
         for(int i = 0; i < str.length(); i++){
-            char var = str.charAt(i);
-            time += getTime(var);
+            str2 += str.charAt(i);
+            alphabet = getCroatiaAlphabet(str2);
+            if(alphabet != ""){
+                str2 = str2.replace(alphabet,"");
+                cnt++;
+            }
         }
-
-
-        bw.write(Integer.toString(time));
+        System.out.println(str2);
+        cnt += str2.length();
+        bw.write(Integer.toString(cnt));
 
         bw.flush();
         bw.close();
         br.close();
     }
+
+    private static String getCroatiaAlphabet(String str2) {
+        String str = "";
+        if(str2.contains("c=")){
+            str = "c=";
+            return str;
+        } else if (str2.contains("c-")) {
+            str = "c-";
+            return str;
+        } else if (str2.contains("dz=")) {
+            str = "dz=";
+            return str;
+        } else if (str2.contains("d-")) {
+            str = "d-";
+            return str;
+        } else if (str2.contains("lj")) {
+            str = "lj";
+            return str;
+        } else if (str2.contains("nj")) {
+            str = "nj";
+            return str;
+        } else if (str2.contains("s=")) {
+            str = "s=";
+            return str;
+        } else if (str2.contains("z=")) {
+            str = "z=";
+            return str;
+        }
+        return str;
+    }
+
     public static int getTime(char a){
         int time = 0;
-        if(a >= 65 && a <= 90){
-            if(a <= 67){ // ABC - 2
-                time = 3;
-            }else if(a <= 70){ // DEF - 3
-                time = 4;
-            }else if(a <= 73){ // GHI - 4
-                time = 5;
-            }else if(a <= 76){ // JKL - 5
-                time = 6;
-            }else if(a <= 79){ // MNO - 6
-                time = 7;
-            }else if(a <= 83){ // PQRS - 7
-                time = 8;
-            }else if(a <= 86){ // TUV - 8
-                time = 9;
-            }else{ // WXYZ - 9
-                time = 10;
-            }
-        }
+
+
         return time;
     }
 }
