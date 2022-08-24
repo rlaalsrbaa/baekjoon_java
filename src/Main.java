4bd6393f653dcee1d9128907e88fc1cd5355b8e1
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-24 23:58:46 +0900

    백준 6단계 1316번 그룹 단어 체커 풀이 1

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,32 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String str = br.readLine();
-
-        String str2 = "";
-        String alphabet = "";
+        int tc = Integer.parseInt(br.readLine());
         int cnt = 0;
 
-        for(int i = 0; i < str.length(); i++){
-            str2 += str.charAt(i);
-            alphabet = getCroatiaAlphabet(str2);
-            if(alphabet != ""){
-                str2 = str2.replace(alphabet,".");
-                cnt++;
+        for(int i = 0; i < tc; i++){
+            String word = br.readLine();
+            List<Character> temp = new ArrayList<>();
+            char var = 0;
+            int checker = 1;
+            for(int j = 0; j < word.length(); j++){
+                if(temp.contains(word.charAt(j)) && var != word.charAt(j)){
+                    checker = 0;
+                }
+                var = word.charAt(j);
+                temp.add(word.charAt(j));
             }
+            cnt += checker;
         }
-
-        str2 = str2.replace(".","");
-        cnt += str2.length();
         bw.write(Integer.toString(cnt));
-
         bw.flush();
         bw.close();
         br.close();
-    }
-
-    private static String getCroatiaAlphabet(String str2) {
-        String str = "";
-        if(str2.contains("c=")){
-            str = "c=";
-            return str;
-        } else if (str2.contains("c-")) {
-            str = "c-";
-            return str;
-        } else if (str2.contains("dz=")) {
-            str = "dz=";
-            return str;
-        } else if (str2.contains("d-")) {
-            str = "d-";
-            return str;
-        } else if (str2.contains("lj")) {
-            str = "lj";
-            return str;
-        } else if (str2.contains("nj")) {
-            str = "nj";
-            return str;
-        } else if (str2.contains("s=")) {
-            str = "s=";
-            return str;
-        } else if (str2.contains("z=")) {
-            str = "z=";
-            return str;
-        }
-        return str;
-    }
-
-    public static int getTime(char a){
-        int time = 0;
-
-
-        return time;
     }
 }
