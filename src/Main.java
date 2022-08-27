344f8e550889a8d48df472708bb6bb979f87ee8f
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-08-28 00:19:33 +0900

    백준 1단계 3003 번 킹, 퀸, 룩, 비숍, 나이트, 폰

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,18 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int temp = 1;
-        int i = 0;
-
-        if (N == 1) {
-            i = 1;
-        } else {
-            while (true) {
-                i++;
-                temp = temp + (6 * i);
-                if (N <= temp) {
-                    i++;
-                    break;
-                }
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = {1,1,2,2,2,8};
+        int var = 0;
+        for(int i = 0; i < 6; i ++){
+            var = Integer.parseInt(st.nextToken());
+            bw.write(Integer.toString(arr[i] - var) + " ");
         }
-        bw.write(Integer.toString(i) + "\n");
 
         bw.flush();
         bw.close();
