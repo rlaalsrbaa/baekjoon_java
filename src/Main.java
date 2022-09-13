7bc59dc2b771cb2d6c63b221251d9a6f11e40107
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-09-14 00:32:02 +0900

    백준 7단계 2839번 설탕 배달 풀이 2

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,17 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         int A = 0;
-        int temp = N % 5;
-        int i = 0;
-        if(temp % 3 == 0){
-            A = (N / 5) + (temp / 3);
-        }
-        while(true){
-            if(temp % 3 == 0){
-                A = (N / 5) + (temp / 3);
-                break;
-            }else if (i == N / 5){
-                break;
-            }
-            i++;
-            temp += (i & 5);
-        }
-        if(N % 3 == 0){
-            A = N / 3;
-        }else{
+
+        if(N == 4 || N == 7){
             A = -1;
+        }else if(N % 5 == 0){
+            A = N / 5;
+        }else if (N % 5 == 1 || N % 5 == 3){
+            A = (N / 5) + 1;
+        }else if (N % 5 == 2 || N % 5 == 4){
+            A = (N / 5) + 2;
         }
+        bw.write(Integer.toString(A));
         bw.flush();
         bw.close();
         br.close();
