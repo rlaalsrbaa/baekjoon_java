6545360b4670e0f2924ebd92b1b47e1a12b81ca7
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-09-16 23:59:24 +0900

    백준 8단계 1978번 소수 찾기 풀이중

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,37 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int cnt = 0;
 
-        BigInteger A = new BigInteger(st.nextToken());
-        BigInteger B = new BigInteger(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            int A = Integer.parseInt(st.nextToken());
+            if(A == 2){
+             cnt++;
+             continue;
+            }
 
-        BigInteger result = A.add(B);
-        bw.write(String.format("%s",result));
+        }
+
+        bw.write(Integer.toString(cnt));
 
         bw.flush();
         bw.close();
         br.close();
+    }
+    public static int decimalChecker(int A){
+        int isdecimal = 0;
+        for(int j = 2; j < A; j++){
+            if(A % j == 0){
+
+                break;
+            }
+        }
+        return isdecimal;
     }
 }
