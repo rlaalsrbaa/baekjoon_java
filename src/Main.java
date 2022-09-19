a541725523745b5ef7d49507fa2e5f5daa2bc222
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-09-19 23:36:56 +0900

    백준 8단계 11653번 소인수 분해 풀이1

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int M = Integer.parseInt(br.readLine());
         int N = Integer.parseInt(br.readLine());
-        boolean isCheckedMinValue = false;
-        int min = 0;
-        int total = 0;
-
-        for (int i = M; i <= N; i++) {
-            if(decimalChecker(i)){
-                total += i;
-                if(!isCheckedMinValue){
-                    min = i;
-                    isCheckedMinValue = true;
+        int i = 2;
+        if (N != 1) {
+            while(true) {
+                if (decimalChecker(i)) {
+                    if (N % i == 0) {
+                        N = N / i;
+                        bw.write(Integer.toString(i) + "\n");
+                        if(N == 1){
+                            break;
+                        }
+                        continue;
+                    }
                 }
+                i++;
             }
-        }
-        if(isCheckedMinValue){
-            bw.write(String.format("%d\n%d",total,min));
-        }else{
-            bw.write(String.format("-1"));
+            bw.flush();
         }
 
 
-        bw.flush();
         bw.close();
         br.close();
     }
-    public static boolean decimalChecker(int A){
+
+    public static boolean decimalChecker(int A) {
         boolean isdecimal = false;
-        if(A == 1){
-            return isdecimal;
-        } else if (A == 2) {
-            return true;
-        }
-        for(int j = 2; j < A; j++){
-            if(A % j == 0){
+
+        for (int j = 2; j < A; j++) {
+            if (A % j == 0) {
                 return isdecimal;
             }
         }
