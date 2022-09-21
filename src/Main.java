d3e61d67757ea30e9803ce37c7685d02594ac2c6
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-09-21 22:10:23 +0900

    백준 8단계 1929번 소수 구하기 풀이1

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,34 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int i = 2;
-        if (N != 1) {
-            while (true) {
-                if (N % i == 0) {
-                    if (decimalChecker(i)) {
-                        N = N / i;
-                        bw.write(Integer.toString(i) + "\n");
-                        if (N == 1) {
-                            break;
-                        }
-                        continue;
-                    }
-                }
-                i++;
-            }
-            bw.flush();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        boolean arr[] = new boolean[N+1];
+
+        for (int i = 2; i <= N; i++) {
+            arr[i] = true;
         }
 
+        for (int i = 2; i * i <= N; i++)
+        {
+            if (arr[i])
+                for (int j = i * i; j <= N; j += i)
+                    arr[j] = false;
+        }
+        for(int i = M; i <= N; i++){
+            if(arr[i]){
+                bw.write(Integer.toString(i)+"\n");
+            }
 
+        }
+
+        bw.flush();
         bw.close();
         br.close();
-    }
-
-    public static boolean decimalChecker(int A) {
-        boolean isdecimal = false;
-
-        for (int j = 2; j < A; j++) {
-            if (A % j == 0) {
-                return isdecimal;
-            }
-        }
-        isdecimal = true;
-        return isdecimal;
     }
 }
