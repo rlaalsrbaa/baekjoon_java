59e7a2985b52aabfe836903039193a485783a1dd
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-09-22 17:02:22 +0900

    백준 8단계 4948번 베르트랑 공준 풀이1

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,41 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        boolean arr[] = new boolean[N+1];
 
-        for (int i = 2; i <= N; i++) {
-            arr[i] = true;
-        }
 
-        for (int i = 2; i * i <= N; i++)
-        {
-            if (arr[i])
-                for (int j = i * i; j <= N; j += i)
-                    arr[j] = false;
-        }
-        for(int i = M; i <= N; i++){
-            if(arr[i]){
-                bw.write(Integer.toString(i)+"\n");
+        while(true){
+            int N = Integer.parseInt(br.readLine());
+            int nn = 2 * N;
+            int cnt = 0;
+
+            boolean arr[] = new boolean[nn+1];
+            if(N == 0){
+                break;
+            }
+            for (int i = 2; i <= nn; i++) {
+                arr[i] = true;
             }
 
+            for (int i = 2; i * i <= nn; i++)
+            {
+                if (arr[i])
+                    for (int j = i * i; j <= nn; j += i)
+                        arr[j] = false;
+            }
+            for(int i = N+1; i <= nn; i++){
+                if(arr[i]){
+                    cnt++;
+                }
+            }
+            bw.write(Integer.toString(cnt)+"\n");
+
+            bw.flush();
         }
 
-        bw.flush();
+
         bw.close();
         br.close();
     }
